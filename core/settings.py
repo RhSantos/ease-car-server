34a476d6b3baafad30e6beaf99870fe41f8117d2
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-19 12:52:27 -0700

    chore: Core settings

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+$cdigie*a4)^om7s#lm^66p0ep$$-1&32sj!bm2w04h&m%#c5'
+SECRET_KEY = 'django-insecure-&c&$8^fxu5#lz7mp3gsx7q%6(d$yyp5zm$rn2hvwxd%+!1oi5a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
