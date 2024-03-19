63f9bfda691ea72b8a2d4a4e199137c296ee7fef
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-19 13:02:40 -0700

    style: Formatting Models.py

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Brand
 
-# Register your models here.
+from .models import Brand, Car
+
 admin.site.register(Brand)
+admin.site.register(Car)
--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,6 @@
 from django.urls import include, path
 
-urlpatterns = [path("brand/", include('api.brand.urls'))]
+urlpatterns = [
+    path("brand/", include("api.brand.urls")),
+    path("car/", include("api.car.urls")),
+]
