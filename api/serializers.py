9460795944acd60d5238e74a2a3cbdf94d13b744
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-19 13:05:51 -0700

    feat: Car Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from .models import Brand
+
+from .models import Brand, Car
+
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = ['id', 'name', 'image']
+        fields = ["id", "name", "image"]
+
+    class Meta:
+        model = Car
+        fields = [
+            "id",
+            "name",
+            "brand",
+            "passengers",
+            "doors",
+            "has_air_conditioning",
+            "has_power_locks",
+            "has_power_windows",
+            "fuel_type",
+            "is_manual",
+            "horsepower",
+            "top_speed",
+            "acceleration_0_100",
+        ]
