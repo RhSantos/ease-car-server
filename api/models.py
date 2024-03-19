14781b40dce86fcf89c1e704b1eaeffb0bde257c
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-19 12:54:15 -0700

    feat: Car Model

--- api/models.py
+++ api/models.py
@@ -4,3 +4,17 @@ class Brand(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='brand_images/')
 
+class Car(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='car_images/')
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    passengers = models.IntegerField()
+    doors = models.IntegerField()
+    has_air_conditioning = models.BooleanField()
+    has_power_locks = models.BooleanField()
+    has_power_windows = models.BooleanField()
+    fuel_type = models.CharField(max_length=50)
+    is_manual = models.BooleanField()
+    horsepower = models.IntegerField()
+    top_speed = models.IntegerField()
+    acceleration_0_100 = models.DecimalField(max_digits=4, decimal_places=2)
