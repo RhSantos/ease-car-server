c9495f482d3730bf0f7d4bc22b95dd5bc141add6
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-20 07:25:20 -0700

    feat: Car URL [GET] assigned in base urls.py

--- api/car/urls.py
+++ api/car/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.get_all_cars, name="get_all_cars")]
+urlpatterns = [
+    path("", views.get_all_cars, name="get_all_cars"),
+    path("<int:car_id>/", views.get_car, name="get_car"),
+]
