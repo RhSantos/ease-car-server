ff59e3084b5a89dac00c57dae715ed9aed711d42
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-20 12:59:24 -0700

    feat: Admin Register - [Address]

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from django.contrib.admin.widgets import AdminFileWidget
-from django.db import models
-from django.utils.safestring import mark_safe
 
-from .models import Brand, Car
+from .models import Brand, Car, Address
 
 
 @admin.register(Brand)
@@ -18,3 +15,11 @@ class CarAdmin(admin.ModelAdmin):
 
     list_display = ["name", "image_preview"]
     readonly_fields = ["image_preview"]
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    all_fields = [f.name for f in Address._meta.fields]
+    parent_fields = Address.get_deferred_fields(Address)
+
+    list_display = all_fields
+    read_only = parent_fields
