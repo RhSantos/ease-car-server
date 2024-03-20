5fa94641d55e7105d939878beb415dfb08dfb1d4
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-20 12:57:54 -0700

    feat: Address Model

--- api/models.py
+++ api/models.py
@@ -51,3 +51,15 @@ class Car(models.Model):
         return mark_safe('<img src="%s" height="200px"/>' % self.image.url)
 
     image_preview.allow_tags = True
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=50, default = "Brasil")
+    complement = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state} {self.postal_code}, {self.country}"
+    
