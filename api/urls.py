d1bdf415b3eed41d34ae5d6b716fb1e3db521b1c
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-20 13:16:16 -0700

    feat: Address Router assigned in API app urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .address.views import AddressViewSet
 from .brand.views import BrandViewSet
 from .car.views import CarViewSet
 
 router = DefaultRouter()
 router.register("car", CarViewSet)
 router.register("brand", BrandViewSet)
+router.register("address", AddressViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
