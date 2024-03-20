8adbd612216d7e5d8647f2206b3f4b3dde13c8b1
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-20 13:10:01 -0700

    core: added Number Field for Address model

--- api/models.py
+++ api/models.py
@@ -52,14 +52,15 @@ class Car(models.Model):
 
     image_preview.allow_tags = True
 
+
 class Address(models.Model):
     street = models.CharField(max_length=100)
+    number = models.IntegerField()
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     postal_code = models.CharField(max_length=20)
-    country = models.CharField(max_length=50, default = "Brasil")
+    country = models.CharField(max_length=50, default="Brasil")
     complement = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.street}, {self.city}, {self.state} {self.postal_code}, {self.country}"
-    
