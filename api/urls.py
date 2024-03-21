4e91b37852b8684b781f180be3b1e0c62dcd30ec
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-21 11:13:03 -0700

    chore: Routes folder for API Collections

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .address.views import AddressViewSet
-from .brand.views import BrandViewSet
-from .car.views import CarViewSet
-from .rental.views import RentalViewSet
+from .routes.address.views import AddressViewSet
+from .routes.brand.views import BrandViewSet
+from .routes.car.views import CarViewSet
+from .routes.rental.views import RentalViewSet
 
 router = DefaultRouter()
 router.register("car", CarViewSet)
