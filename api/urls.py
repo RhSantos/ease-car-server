d13d0f82100db4bd108fd32ff0e93a8b047a49f8
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-21 07:43:25 -0700

    feat: Rental Router assigned in API app urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,13 @@ from rest_framework.routers import DefaultRouter
 from .address.views import AddressViewSet
 from .brand.views import BrandViewSet
 from .car.views import CarViewSet
+from .rental.views import RentalViewSet
 
 router = DefaultRouter()
 router.register("car", CarViewSet)
 router.register("brand", BrandViewSet)
 router.register("address", AddressViewSet)
+router.register("rental", RentalViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
