7fb6a4f0d62f540a87a07b8413ad8a2662f356a9
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-21 11:20:48 -0700

    feat: Admin Register - [Review]

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Address, Brand, Car, Rental
+from .models import Address, Brand, Car, Rental, Review
 
 
 @admin.register(Brand)
@@ -30,6 +30,15 @@ class AddressAdmin(admin.ModelAdmin):
 class RentalAdmin(admin.ModelAdmin):
     all_fields = [f.name for f in Rental._meta.fields]
     parent_fields = Rental.get_deferred_fields(Rental)
+
+    list_display = all_fields
+    read_only = parent_fields
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    all_fields = [f.name for f in Review._meta.fields]
+    parent_fields = Review.get_deferred_fields(Review)
 
     list_display = all_fields
     read_only = parent_fields
