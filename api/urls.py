efbeb05c5ce4f563e772ffa3bff30f12b9432342
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-21 11:24:46 -0700

    feat: Review Router assigned in API app urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,14 @@ from .routes.address.views import AddressViewSet
 from .routes.brand.views import BrandViewSet
 from .routes.car.views import CarViewSet
 from .routes.rental.views import RentalViewSet
+from .routes.review.views import ReviewViewSet
 
 router = DefaultRouter()
 router.register("car", CarViewSet)
 router.register("brand", BrandViewSet)
 router.register("address", AddressViewSet)
 router.register("rental", RentalViewSet)
+router.register("review", ReviewViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
