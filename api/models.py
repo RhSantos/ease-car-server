b91a98d703aa41f2303fb534e0ba17721057e07c
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-23 18:00:21 -0300

    feat: Booking Model

--- api/models.py
+++ api/models.py
@@ -154,3 +154,22 @@ class Payment(models.Model):
 
         self.updated_at = timezone.now()
         super(Payment, self).save(*args, **kwargs)
+
+
+class Booking(models.Model):
+
+    renter = models.ForeignKey(User, on_delete=models.CASCADE)
+    rental = models.ForeignKey(Rental, on_delete=models.CASCADE)
+    location = models.ForeignKey(Address, on_delete=models.CASCADE)
+    rent_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    payments = models.ManyToManyField(Payment)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        super(Booking, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.id} [{self.renter} - {self.rental}]"
