de3201bd429d033ca81f835fd177c4a7c00f2f84
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-25 15:19:15 -0700

    chore: Review Jsend Responses

--- api/routes/review/views.py
+++ api/routes/review/views.py
@@ -1,8 +1,10 @@
+from django.http.response import Http404
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from api.models import Review
 from api.serializers import ReviewSerializer
+from api.utils.jsend_responses import error_response, fail_response, success_response
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
@@ -10,40 +12,44 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
 
     def list(self, request):
-        review = Review.objects.all()
-        serializer = ReviewSerializer(review, many=True)
-        return Response(serializer.data)
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return success_response({"reviews": serializer.data})
 
     def retrieve(self, request, pk=None):
         try:
             review = self.get_object()
             serializer = ReviewSerializer(review)
             return Response(serializer.data)
-        except Review.DoesNotExist:
-            return Response({"error": "Review not found"}, status=404)
+        except Exception as e:
+            print(e.__class__)
+            return error_response("Review not found")
 
     def create(self, request):
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return success_response(
+                {"review": serializer.data}, status=status.HTTP_201_CREATED
+            )
+        return fail_response(serializer.errors)
 
     def update(self, request, pk=None):
         try:
             review = self.get_object()
-        except Review.DoesNotExist:
-            return Response(
-                {"error": "Review not found"}, status=status.HTTP_404_NOT_FOUND
-            )
+        except Http404:
+            return error_response("Review not found")
 
         serializer = ReviewSerializer(review, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return fail_response(serializer.errors)
 
     def destroy(self, request, pk=None):
-        review = self.get_object()
-        review.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            review = self.get_object()
+            review.delete()
+        except Http404:
+            return error_response("Review not found")
+        return success_response()
