d83375527feb2a7e4f8d362f8cfe0845ab552126
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-25 15:19:36 -0700

    chore: Booking Jsend Responses

--- api/routes/booking/views.py
+++ api/routes/booking/views.py
@@ -1,8 +1,10 @@
+from django.http.response import Http404
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from api.models import Booking
 from api.serializers import BookingSerializer
+from api.utils.jsend_responses import error_response, fail_response, success_response
 
 
 class BookingViewSet(viewsets.ModelViewSet):
@@ -10,40 +12,44 @@ class BookingViewSet(viewsets.ModelViewSet):
     serializer_class = BookingSerializer
 
     def list(self, request):
-        booking = Booking.objects.all()
-        serializer = BookingSerializer(booking, many=True)
-        return Response(serializer.data)
+        bookings = Booking.objects.all()
+        serializer = BookingSerializer(bookings, many=True)
+        return success_response({"bookings": serializer.data})
 
     def retrieve(self, request, pk=None):
         try:
             booking = self.get_object()
             serializer = BookingSerializer(booking)
             return Response(serializer.data)
-        except Booking.DoesNotExist:
-            return Response({"error": "Booking not found"}, status=404)
+        except Exception as e:
+            print(e.__class__)
+            return error_response("Booking not found")
 
     def create(self, request):
         serializer = BookingSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return success_response(
+                {"booking": serializer.data}, status=status.HTTP_201_CREATED
+            )
+        return fail_response(serializer.errors)
 
     def update(self, request, pk=None):
         try:
             booking = self.get_object()
-        except Booking.DoesNotExist:
-            return Response(
-                {"error": "Booking not found"}, status=status.HTTP_404_NOT_FOUND
-            )
+        except Http404:
+            return error_response("Booking not found")
 
         serializer = BookingSerializer(booking, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return fail_response(serializer.errors)
 
     def destroy(self, request, pk=None):
-        booking = self.get_object()
-        booking.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            booking = self.get_object()
+            booking.delete()
+        except Http404:
+            return error_response("Booking not found")
+        return success_response()
