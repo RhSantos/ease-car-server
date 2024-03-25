6eb0f00cbd59dd89620e6250761914aa8a01fce6
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-25 15:19:08 -0700

    chore: Rental Jsend Responses

--- api/routes/rental/views.py
+++ api/routes/rental/views.py
@@ -1,8 +1,10 @@
+from django.http.response import Http404
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from api.models import Rental
 from api.serializers import RentalSerializer
+from api.utils.jsend_responses import error_response, fail_response, success_response
 
 
 class RentalViewSet(viewsets.ModelViewSet):
@@ -10,40 +12,44 @@ class RentalViewSet(viewsets.ModelViewSet):
     serializer_class = RentalSerializer
 
     def list(self, request):
-        rental = Rental.objects.all()
-        serializer = RentalSerializer(rental, many=True)
-        return Response(serializer.data)
+        rentals = Rental.objects.all()
+        serializer = RentalSerializer(rentals, many=True)
+        return success_response({"rentals": serializer.data})
 
     def retrieve(self, request, pk=None):
         try:
             rental = self.get_object()
             serializer = RentalSerializer(rental)
             return Response(serializer.data)
-        except Rental.DoesNotExist:
-            return Response({"error": "Rental not found"}, status=404)
+        except Exception as e:
+            print(e.__class__)
+            return error_response("Rental not found")
 
     def create(self, request):
         serializer = RentalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return success_response(
+                {"rental": serializer.data}, status=status.HTTP_201_CREATED
+            )
+        return fail_response(serializer.errors)
 
     def update(self, request, pk=None):
         try:
             rental = self.get_object()
-        except Rental.DoesNotExist:
-            return Response(
-                {"error": "Rental not found"}, status=status.HTTP_404_NOT_FOUND
-            )
+        except Http404:
+            return error_response("Rental not found")
 
         serializer = RentalSerializer(rental, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return fail_response(serializer.errors)
 
     def destroy(self, request, pk=None):
-        rental = self.get_object()
-        rental.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            rental = self.get_object()
+            rental.delete()
+        except Http404:
+            return error_response("Rental not found")
+        return success_response()
