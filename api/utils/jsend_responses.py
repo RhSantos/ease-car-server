a3ec1d8faff1615b8920ed2bab7c9a717920a061
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-25 15:12:06 -0700

    chore: HTTP Status in Jsend Responses

--- api/utils/jsend_responses.py
+++ api/utils/jsend_responses.py
@@ -1,13 +1,13 @@
 from rest_framework.response import Response
+from rest_framework import status
+
+def success_response(data=None, status=status.HTTP_200_OK):
+    return Response({"status": "success", "data": data}, status = status)
 
 
-def success_response(data):
-    return Response({"status": "success", "data": data})
+def fail_response(errors, status = status.HTTP_400_BAD_REQUEST):
+    return Response({"status": "fail", "data": errors}, status = status)
 
 
-def fail_response(errors):
-    return Response({"status": "fail", "data": errors})
-
-
-def error_response(message):
-    return Response({"status": "error", "message": message})
+def error_response(message, status = status.HTTP_404_NOT_FOUND):
+    return Response({"status": "error", "message": message}, status = status)
