d0769436eccadafe8b8b9df0a405a80da5515a3e
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-25 15:19:25 -0700

    chore: Favorite Jsend Responses

--- api/routes/favorite/views.py
+++ api/routes/favorite/views.py
@@ -1,8 +1,10 @@
+from django.http.response import Http404
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from api.models import Favorite
 from api.serializers import FavoriteSerializer
+from api.utils.jsend_responses import error_response, fail_response, success_response
 
 
 class FavoriteViewSet(viewsets.ModelViewSet):
@@ -10,40 +12,44 @@ class FavoriteViewSet(viewsets.ModelViewSet):
     serializer_class = FavoriteSerializer
 
     def list(self, request):
-        favorite = Favorite.objects.all()
-        serializer = FavoriteSerializer(favorite, many=True)
-        return Response(serializer.data)
+        favorites = Favorite.objects.all()
+        serializer = FavoriteSerializer(favorites, many=True)
+        return success_response({"favorites": serializer.data})
 
     def retrieve(self, request, pk=None):
         try:
             favorite = self.get_object()
             serializer = FavoriteSerializer(favorite)
             return Response(serializer.data)
-        except Favorite.DoesNotExist:
-            return Response({"error": "Favorite not found"}, status=404)
+        except Exception as e:
+            print(e.__class__)
+            return error_response("Favorite not found")
 
     def create(self, request):
         serializer = FavoriteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return success_response(
+                {"favorite": serializer.data}, status=status.HTTP_201_CREATED
+            )
+        return fail_response(serializer.errors)
 
     def update(self, request, pk=None):
         try:
             favorite = self.get_object()
-        except Favorite.DoesNotExist:
-            return Response(
-                {"error": "Favorite not found"}, status=status.HTTP_404_NOT_FOUND
-            )
+        except Http404:
+            return error_response("Favorite not found")
 
         serializer = FavoriteSerializer(favorite, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return fail_response(serializer.errors)
 
     def destroy(self, request, pk=None):
-        favorite = self.get_object()
-        favorite.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            favorite = self.get_object()
+            favorite.delete()
+        except Http404:
+            return error_response("Favorite not found")
+        return success_response()
