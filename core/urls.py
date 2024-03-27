32069d02777a1d9920c4238cb0b2d95a872f2528
RhSantos <rhuan.santos.araujo@gmail.com>
2024-03-27 15:45:00 -0300

    chore: Simple JWT URL

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
+    path("auth/token/", TokenObtainPairView.as_view()),
+    path("auth/token/refresh/", TokenRefreshView.as_view()),
     path("api/", include("api.urls")),
 ]
 
